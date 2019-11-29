0c88ff28dafbd505a9f28dccbf3155d1f4e7e384
Aleksandr Sigov <paperrcutt@gmail.com>
2019-11-29 19:51:57 +0300

    StickerTest

---
+++ src/test/java/StickerTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class StickerTest extends BaseTest {
+
+    @Test
+    public void testInstance() {
+        driver.get("http://localhost/litecart/en/");
+        List<WebElement> elements = driver.findElements(By.className("image-wrapper"));
+        for (WebElement e : elements) {
+            Assert.assertTrue(e.findElements(By.className("sticker")).size() == 1);
+        }
+    }
+}
